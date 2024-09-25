69567a09cc32e91ec051c448ba59f89773e14464
Ansh Mehta <anshkapilmehta@gmail.com>
2024-09-25 23:43:28 +0530

    phonesvg

--- src/app/components/What.jsx
+++ src/app/components/What.jsx
@@ -2,19 +2,17 @@ import Image from "next/image";
 
 export default function What() {
   return (
-    <section className="bg-background max-w-screen h-screen flex flex-row justify-evenly">
-      <div className="w-1/2 border-borderGreen border-r-2 border-t-2 border-b-2  flex justify-center items-center">
+    <section className="bg-background max-w-screen h-screen flex flex-col md:flex-row md:justify-evenly">
+      <div className="w-1/2 border-borderGreen border-r-2 border-t-2 border-b-2  hidden md:flex justify-center items-center">
         <Image src="laptop.svg" alt="laptop" width={650} height={650} />
       </div>
-      <div className="w-1/2 border-borderGreen border-l-2 border-t-2 border-b-2 flex justify-start items-center pr-[10vw]">
-        <div className="flex flex-col ml-[5vw]">
-          <h1 className="text-lightGreen text-5xl my-6">
-            WHAT'S{" "}
-            <span className="text-[#E9FAC1]">
-              <br></br>ASSIGNOFAST?
-            </span>
+      <div className="w-full h-full md:w-1/2 border-borderGreen md:border-l-2 md:border-t-2 md:border-b-2 flex justify-start md:items-center md:pr-[10vw]">
+        <div className="flex flex-col mx-[7.5vw] md:ml-[5vw] md:mx-0">
+          <h1 className="text-lightGreen text-2xl text-center md:text-5xl my-6">
+            WHAT'S <span className="hidden md:inline-block">{"\n"}</span>
+            <span className="text-[#E9FAC1]">ASSIGNOFAST?</span>
           </h1>
-          <p className="text-2xl uppercase">
+          <p className=" text-sm md:text-2xl uppercase text-justify md:text-left">
             <span className="text-lightGreen">Assignofast</span> is more than
             just a regular reminder app. It taps into the digital world of
             assignments posted by your faculty, automatically loading them into
@@ -24,6 +22,9 @@ export default function What() {
             notifications come even more frequently, ensuring you stay on top of
             your work.
           </p>
+          <div className=" md:hidden w-full flex items-center justify-center border-red-800 border-2">
+            <Image src="phone.svg" alt="laptop" width={500} height={500} />
+          </div>
         </div>
       </div>
     </section>
