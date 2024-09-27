5f980da696c63475035b7ae0119a95d54fc30256
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-09-27 21:18:28 +0530

    feat: navbar

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,12 @@ import Landing from "./components/landing";
 import What from "./components/What";
 import Features from "./components/Features";
 import Why from "./components/Why";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Landing />
       <What />
       {/* <Features /> */}
