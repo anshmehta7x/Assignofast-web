b74383d9b5f3bde2ff1cd2f3af516eb7336b6a00
Ansh Mehta <anshkapilmehta@gmail.com>
2024-10-23 22:28:15 +0530

    new logo

--- src/app/components/Features.jsx
+++ src/app/components/Features.jsx
@@ -1,22 +1,41 @@
-'use client';
+"use client";
 import Image from "next/image";
 import { Abyssinica_SIL, Alexandria } from "next/font/google";
 
 const abyssinica = Abyssinica_SIL({
-  weight: '400',
-  subsets: ['latin'],
+  weight: "400",
+  subsets: ["latin"],
 });
 
 const alexandria = Alexandria({
-  weight: '400',
-  subsets: ['latin'],
+  weight: "400",
+  subsets: ["latin"],
 });
 
 const featuresData = [
-  { icon: "./icon-box.svg", title: "Single avenue", content: "All your deadlines at a single place, helping you stay organized and never miss important tasks." },
-  { icon: "./updated.svg", title: "Customised Tasks", content: "Safe and seamless remainder options." },
-  { icon: "./sync.svg", title: "Auto Sync", content: "All your assignments are automatically synced from your online portal, keeping everything organized and accessible." },
-  { icon: "./reminder.svg", title: "Personal reminders", content: "You can use the app to add personal tasks to refrain from forgetting your personal occupations" }
+  {
+    icon: "./icon-box.svg",
+    title: "Single avenue",
+    content:
+      "All your deadlines at a single place, helping you stay organized and never miss important tasks.",
+  },
+  {
+    icon: "./updated.svg",
+    title: "Customised Tasks",
+    content: "Safe and seamless remainder options.",
+  },
+  {
+    icon: "./sync.svg",
+    title: "Auto Sync",
+    content:
+      "All your assignments are automatically synced from your online portal, keeping everything organized and accessible.",
+  },
+  {
+    icon: "./reminder.svg",
+    title: "Personal reminders",
+    content:
+      "You can use the app to add personal tasks to refrain from forgetting your personal occupations",
+  },
 ];
 
 export default function Features() {
@@ -35,25 +54,32 @@ export default function Features() {
         <div className="flex flex-col justify-center text-white w-full md:w-1/2">
           <div className="grid grid-cols-2 gap-4 md:gap-8">
             {featuresData.map((feature, index) => (
-              <div key={index} className="flex flex-col items-center md:items-start text-center md:text-left p-2">
-                <img src={feature.icon} alt={feature.title} className="w-12 h-12 mb-2" />
-                <h2 className="text-lg md:text-xl mb-1 font-bold">{feature.title}</h2>
+              <div
+                key={index}
+                className="flex flex-col items-center md:items-start text-center md:text-left p-2"
+              >
+                <img
+                  src={feature.icon}
+                  alt={feature.title}
+                  className="w-12 h-12 mb-2"
+                />
+                <h2 className="text-lg md:text-xl mb-1 font-bold">
+                  {feature.title}
+                </h2>
                 <p className="text-sm md:text-base">{feature.content}</p>
               </div>
             ))}
           </div>
         </div>
-        
       </div>
-      
-      <div className="flex items-center justify-center w-full lg:-mt-[20vh]">
-  <button className="bg-[#465254] w-[35vw] lg:w-[20vw] h-[10vh] rounded-full border-4 border-[#6da154] flex items-center justify-center">
-    <div className="bg-[#070707] w-[33vw] lg:w-[18vw] h-[7vh] rounded-full font-bold border-2 border-[#6da154] flex items-center justify-center">
-      USER GUIDE
-    </div>
-  </button>
-</div>
 
+      <div className="flex items-center justify-center w-full lg:mt-[5vh]">
+        <button className="bg-[#465254] w-[35vw] lg:w-[20vw] h-[10vh] rounded-full border-4 border-[#6da154] flex items-center justify-center">
+          <div className="bg-[#070707] w-[33vw] lg:w-[18vw] h-[7vh] rounded-full font-bold border-2 border-[#6da154] flex items-center justify-center">
+            USER GUIDE
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
