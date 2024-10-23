82d44d631b3f577e5113aaed0122dc97bd5c805e
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-10-23 23:11:49 +0530

    landing

--- src/app/components/landing.jsx
+++ src/app/components/landing.jsx
@@ -2,46 +2,61 @@ import Image from "next/image";
 import { Alexandria } from "next/font/google";
 
 const alexandria = Alexandria({
-  weight: '400',
-  subsets: ['latin'],
+  weight: "400",
+  subsets: ["latin"],
 });
 
 export default function Landing() {
   return (
-    <div className={`h-full w-full bg-black overflow-hidden font-alexandria ${alexandria.className}`}>
-      <div className="flex flex-col mt-[10vh] lg:mt-[20vh] text-[8vh] mx-[5vw] lg:text-[12vh]">
+    <div className={`h-[150vh] lg:h-full w-full bg-black overflow-hidden font-alexandria ${alexandria.className}`}>
+      <div className="flex flex-col mt-[10vh] lg:mt-[30vh] text-[8vh] mx-[5vw] lg:text-[12vh]">
         <p>WE ARE</p>
-        <p className="self-end text-right lg:self-start lg:text-left">IEEE-CS</p>
-        <p className="text-[0vh] lg:text-[12vh] lg:self-end lg:text-right text-center">Assignofast</p>
-        
-        <div className="flex flex-row items-center">
-          <p className="w-[28vw] text-[1vh] lg:text-[3vh]">
-            Assignofast is an app developed by IEEE-CS VIT Chapter to cater to students' necessity of managing their DA’s.
-            <button className="mt-6 py-2 flex items-center justify-start bg-[#89ef00] text-black px-2 w-[10vw]">
-            <img src="link-arrow-right.svg" alt="Arrow Right" className="h-4 w-4 mr-1" />
-            Download
-          </button></p>
-          <div className="w-full max-w-[30vw] relative h-[60vh]">
-  <div className="w-[20%] my-[8%] absolute ml-[40%] inset-0 bg-gradient-to-r from-[#08eb2a] to-transparent rounded-full blur-[8rem] opacity-80"></div>
-  <Image
-    src="/landing-tag.svg"
-    layout="fill"
-    className="relative z-10 object-cover object-top"
-  />
-</div>
-
-
-
-
-
-
-
-        <p className="w-1/3 text-[1vh] lg:text-[3vh] mt-[5vh]">
-          Ground-breaking app for your assignment needs.
+        <p className="self-end text-right lg:self-start lg:text-left lg:text-[10vh]">IEEE-CS</p>
+        <p className="text-[0vh] lg:text-[12vh] lg:self-end lg:text-right text-center -mt-[30vh]">Assignofast</p>
+        <p className="w-1/3 text-[2.5vh] lg:self-end lg:text-right text-center">
+          Your personal shield against deadline dread - Assignofast keeps you always ahead
         </p>
+        <div className="flex flex-row items-center justify-between mt-[40vh] lg:mt-[10vh]">
+          {/* Buttons */}
+          <div className="flex flex-col items-center gap-6 w-[30vw]">
+            <button className="h-[10vh] w-full bg-[#144c11] flex items-center gap-4 p-4">
+              <Image src="/chrome-web.svg" width={45} height={45} alt="Chrome Icon" className="h-full" />
+              <div className="flex flex-col justify-center text-left">
+                <span className="text-[2.5vh]">Available in the</span>
+                <span className="text-[4vh] font-bold">Chrome Web Store</span>
+              </div>
+            </button>
+
+            <div className="flex flex-row gap-4 w-full">
+              <button className="h-[10vh] w-[50%] bg-[#144c11] flex items-center gap-4 p-4">
+                <Image src="/apple.svg" width={45} height={45} alt="Apple Icon" className="h-full" />
+                <div className="flex flex-col justify-center text-left">
+                  <span className="text-[2vh]">Download on the</span>
+                  <span className="text-[3vh] font-bold">App Store</span>
+                </div>
+              </button>
+
+              <button className="h-[10vh] w-[50%] bg-[#144c11] flex items-center gap-4 p-4">
+                <Image src="/google-play.svg" width={45} height={45} alt="Google Play Icon" className="h-full" />
+                <div className="flex flex-col justify-center text-left">
+                  <span className="text-[2vh]">Get it on</span>
+                  <span className="text-[3vh] font-bold">Google Play</span>
+                </div>
+              </button>
+            </div>
+          </div>
+
+          {/* Landing Tag */}
+          <div className="-mt-[75vh] w-[85vw] lg:w-[40vw] h-[50vh] lg:ml-[25%] lg:-mt-[25%] lg:h-[85vh] absolute">
+            <div className="w-[20%] absolute ml-[35%] my-[8%] inset-0 bg-gradient-to-r from-[#08eb2a] to-transparent rounded-full blur-[8rem] opacity-80"></div>
+            <Image src="/landing-tag.svg" layout="fill" className="relative z-10 object-cover object-top" />
+          </div>
         </div>
 
         
+
+        <p className="text-[5vh] lg:text-[0vh] -mt-[150vh] text-center lg:mt-0 ">Assignofast</p>
+        <p className="text-[2vh] text-center lg:text-[0vh]">Assignofast is an app developed by IEEE-CS VIT Chapter to cater to students necessity of managing their DA’s.</p>
       </div>
     </div>
   );
